603ff493c3adfad211ecf9c6b47edb7be7304673
shashidharbhat000@gmail.com <shashidharbhat000@gmail.com>
2024-12-26 12:54:33 +0530

    Greeting

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,17 @@
-
+//import Header from "./Header";
+// import Home from "./Home";
+// import Contact from "./Contact";
+// import About from "./About";
+import Greeting from "./Greeting";
 function App() {
   return (
+    <div>
     <h1>Hello From React</h1>
+    <Greeting name = {'Akash'}/>
+    <Greeting name = {'Ayush'}/>
+ 
+
+    </div>
   );
 }
 
